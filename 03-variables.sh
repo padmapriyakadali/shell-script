f37c9d092f2a713a6603702d159bab3d478a340d
padmapriyakadali <padmapriyakadali@gmail.com>
2024-12-31 00:37:33 +0530

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ZARA
+PERSON2=ZIVA
+
+echo "$PERSON1 :: hello $PERSON2, how are you ?"
+echo "$PERSON2 :: Hi $PERSON1, I' good. How are you ?"
+echo "$PERSON1 :: shall we go for a coffee ?"
+echo "$PERSON2 :: yeah $PERSON1!! sure."
