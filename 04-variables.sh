53dd195daa8ba4cb4619995fd8a63618bf228189
padmapriyakadali <padmapriyakadali@gmail.com>
2024-12-31 00:41:59 +0530

    conversation added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: hello $PERSON2, how are you ?"
+echo "$PERSON2 :: Hi $PERSON1, I' good. How are you ?"
+echo "$PERSON1 :: shall we go for a coffee ?"
+echo "$PERSON2 :: yeah $PERSON1!! sure."
