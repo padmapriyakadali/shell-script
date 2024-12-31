5782acaeda52ba3e4f64bb35227b7855ef29e45d
padmapriyakadali <padmapriyakadali@gmail.com>
2024-12-31 21:43:55 +0530

    printing arrays

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,6 +6,7 @@ echo "script name :: $0"
 echo "Present working directory :: $PWD"
 echo "User's home directory :: $HOME"
 echo "Which user is running the script :: $USER  "
+sleep 5 
 echo "Process ID of the current script :: $$"
-sleep 60
+sleep 10 &
 echo "Process ID of last command in background :: $!" 
