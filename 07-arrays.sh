e8b0fed00b418751985539540d76fd2b3bccc633
padmapriyakadali <padmapriyakadali@gmail.com>
2024-12-31 21:20:36 +0530

    printing arrays

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=$["pushpa" "RRR" "salar"]
+
+echo "First movie is :: $MOVIES[0]"
+echo "Second movie is :: $MOVIES[1]"
+echo "Third movie is :: $MOVIES[2]"
+echo "All movies are :: $MOVIES[@]"
+echo "Number of movies :: $MOVIES[#]"
