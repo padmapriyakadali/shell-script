9e77ba890275afb32e05bbeed71305e1e04e469b
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-01 18:33:29 +0530

    Installing script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,6 +7,8 @@ if [ $USERID -ne 0 ]
     then 
         echo "Error: you must have root priviliges to install"
 else 
+dnf list installed mysql 
+if [ $? -ne 0 ]
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
@@ -17,6 +19,10 @@ else
     fi
         echo "Mysql is already installed"
 fi
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
 dnf install git -y
     if [ $? -ne 0 ]
     then
@@ -25,3 +31,5 @@ dnf install git -y
     else
         echo "installation is success"
     fi
+echo "Mysql is already installed"
+fi
