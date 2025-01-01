53b09f094427466bb8e1bf751a5b910a0d868036
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-01 18:17:57 +0530

    Installing script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,7 +7,7 @@ if [ $USERID -ne 0 ]
     then 
         echo "Error: you must have root priviliges to install"
 else 
-    dnf install mysqll -y
+    dnf install mysql -y
     if [ $? -ne 0 ]
     then
         echo "Installation is not done"
