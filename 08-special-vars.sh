45e008726944c55fefd3621c0e559469785745d0
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-01 11:33:53 +0530

    printing special variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,8 +5,7 @@ echo "Number of variables passed :: $#"
 echo "script name :: $0"
 echo "Present working directory :: $PWD"
 echo "User's home directory :: $HOME"
-echo "Which user is running the script :: $USER  "
-sleep 5 
+echo "Which user is running the script :: $USER"
 echo "Process ID of the current script :: $$"
 sleep 10 &
 echo "Process ID of last command in background :: $!" 
