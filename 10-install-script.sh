ac8031e253500dd1134e16dfc4d41ef65c2d5c60
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-02 13:27:22 +0530

    Installing script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-dnf install mysql -y
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+
+then 
+    echo " you must have root priviliges to execute the script "
+else 
+    dnf install mysql -y
+fi
