cdeb6e8bb7ce179d3aa23432779eb818c9bebf81
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-07 14:57:29 +0530

    Displaying colors

---
+++ 12-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+        then 
+            echo -e "$2 ... is $R failure"
+            exit 1 
+        else 
+            echo -e "$2 ... is $G success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+    then
+        echo "You must have root credentials to execute the script"
+        exit 1
+fi
+
+dnf list installed mysql
+   if [ $? -ne 0 ]
+    then   
+        dnf install mysql -y
+        VALIDATE $? "Installing Mysql"
+    else 
+        echo "MySql is already installed"
+   fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+    then  
+        dnf install git -y
+        VALIDATE $? "Installing git"
+    else 
+        echo "Git is already installed"
+ fi
