aaab13db9a9030b81d398e9ee5a1a95162101c35
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-07 21:26:31 +0530

    executing loops

---
+++ 15-loops.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)    # $0-script name
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+        then 
+            echo -e "$2 ... is $R failure $N"
+            exit 1 
+        else 
+            echo -e "$2 ... is $G success $N"
+    fi
+}
+
+echo "Script started executing at :: $TIMESTAMP" &>>$LOG_FILE_NAME  # 1- success; 2- failure; & - for both success and failure output
+
+if [ $USERID -ne 0 ]
+    then
+        echo "You must have root credentials to execute the script"
+        exit 1
+fi
+
+for package in $@
+do 
+    dnf list installed $package &>>$LOG_FILE_NAME
+    if [ $? -ne 0 ]
+        then 
+            echo "$package is already installed"
+        else
+            dnf install $package -y &>>$LOG_FILE_NAME
+            VALIDATE $? "installating $package"
+done
