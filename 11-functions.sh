f2ef6b95f7cd0ca7449995c702cb4ccd7b1e506c
padmapriyakadali <padmapriyakadali@gmail.com>
2025-01-07 11:55:48 +0530

    Executing functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,12 +2,6 @@
 
 USERID=$(id -u)
 
-if [ $? -ne 0 ]
-    then
-        echo "You must have root credentials to execute the script"
-        exit 1
-fi
-
 VALIDATE(){
 
     if [ $1 -ne 0 ]
@@ -18,6 +12,12 @@ VALIDATE(){
             echo "$2 ... is success"
     fi
 }
+
+if [ $? -ne 0 ]
+    then
+        echo "You must have root credentials to execute the script"
+        exit 1
+fi
 
 dnf list installed mysql
    if [ $? -ne 0 ]
